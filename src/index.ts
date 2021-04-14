b14c3196ba72df1d3cb838be94b408eaa5b19db1
idleberg <git@idleberg.com>
2021-04-14 21:50:53 +0200

    fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -24,54 +24,16 @@ export default class DeveloperConsole {
   private __console__(type: string, ...args: any | any[]): void {
     if (!atom?.inDevMode()) return;
 
-    args.unshift(`%c${this.name}%c`, this.styleSheet, '')
-
-    if (Array.isArray(args)) {
-      (window as any).console[type](...args);
-    } else {
-      args.join(' ');
-      (window as any).console[type](args);
-    }
-  }
-
-  assert(condition?: boolean, ...data: any[]): void {
-    this.__console__('assert', condition, ...data);
-  }
-
-  count(label?: string): void {
-    this.__console__('count', label);
-  }
-
-  countReset(label?: string): void {
-    this.__console__('countReset', label);
+    args.unshift(`%c${this.name}%c`, this.styleSheet, '');
+    (window as any).console[type](...args);
   }
 
   debug(...data: any[]): void {
     this.__console__('debug', ...data);
   }
 
-  dir(data: unknown): void {
-    this.__console__('dir', data);
-  }
-
-  dirxml(data: unknown): void {
-    this.__console__('dirxml', data);
-  }
-
   error(...data: any[]): void {
     this.__console__('error', ...data);
-  }
-
-  group(label?: string): void {
-    this.__console__('group', label);
-  }
-
-  groupCollapsed(label?: string): void {
-    this.__console__('groupCollapsed', label);
-  }
-
-  groupEnd(): void {
-    this.__console__('groupEnd');
   }
 
   info(...data: any[]): void {
@@ -80,22 +42,6 @@ export default class DeveloperConsole {
 
   log(...data: any[]): void {
     this.__console__('log', ...data);
-  }
-
-  table(tabularData?: unknown): void {
-    this.__console__('table', tabularData);
-  }
-
-  time(label?: string): void {
-    this.__console__('time', label);
-  }
-
-  timeEnd(label?: string): void {
-    this.__console__('timeEnd', label);
-  }
-
-  timeLog(label?: string): void {
-    this.__console__('timeLog', label);
   }
 
   trace(...data: any[]): void {
